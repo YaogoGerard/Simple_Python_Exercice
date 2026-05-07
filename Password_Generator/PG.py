9f47e35f4939976a29b676c0f6e4fc85fb0951a4
YaogoGerard <gerardwyaogo@gmail.com>
2026-05-07 12:38:21 +0000

    begin to create password generator

---
+++ Password_Generator/PG.py
@@ -0,0 +1,10 @@
+#  https://github.com/YaogoGerard
+"""
+This program is a password generator with a graphical user interface
+"""
+
+import customtkinter as ctk
+import secrets
+import string
+import pyperclip
+
