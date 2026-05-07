9c6fd210d0ae405d18a0ddc4bb413f61092c8ee1
YaogoGerard <gerardwyaogo@gmail.com>
2026-05-07 02:56:42 +0000

    creation of program madlib

---
+++ Madlibs/madlib.py
@@ -0,0 +1,14 @@
+# https://github.com/YaogoGerard
+"""
+This a simple program that replace user input in a specific segment of a story.
+"""
+
+Adj = input("Please enter an adjectif:")
+verb1 = input("Please enter a verb:")
+verb2 = input("Please enter another verb:")
+your_name = input("Please enter your name:")
+
+
+madlib = f"{your_name} is so {Adj} that they will {verb1} the entire internet and {verb2} until they become the best computer scientist in BURKINA FASO!"
+
+print(madlib)
